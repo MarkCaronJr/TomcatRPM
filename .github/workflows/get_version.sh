cde5579624f2556785972497b8a9507e39b27aa3
Mike Neilson <michael.a.neilson@usace.army.mil>
2021-01-12 10:38:25 -0800

    added code to get FILE name for asset upload action.

--- .github/workflows/get_version.sh
+++ .github/workflows/get_version.sh
@@ -5,5 +5,6 @@ MINOR=`grep minor_version tomcat.spec | head -1 | awk '{ print $3 }'`
 PATCH=`grep patch_version tomcat.spec | head -1 | awk '{ print $3 }'`
 RELEASE=`grep Release: tomcat.spec    | sed "s/Release:\s\+\([0-9]*\).*/\1/"`
 VER="$MAJOR.$MINOR.$PATCH-$RELEASE"
-
+FILE=`ls *.rpm`
 echo "::set-output name=VERSION::$VER"
+ehco "::set-output name=$FILE"
