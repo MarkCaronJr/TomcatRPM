ae3de90a27b4ba5ce1517dbf5d57d5e50b271779
Mike Neilson <michael.a.neilson@usace.army.mil>
2021-01-12 08:47:47 -0800

    created system for creating a release.

---
+++ .github/workflows/get_version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+MAJOR=`grep major_version tomcat.spec | head -1 | awk '{ print $3 }'`
+MINOR=`grep minor_version tomcat.spec | head -1 | awk '{ print $3 }'`
+PATCH=`grep patch_version tomcat.spec | head -1 | awk '{ print $3 }'`
+RELEASE=`grep Release: tomcat.spec    | sed "s/Release:\s\+\([0-9]*\).*/\1/"`
+VER="$MAJOR.$MINOR.$PATCH-$RELEASE"
+
+echo "::set-output name=VERSION::$VER"
